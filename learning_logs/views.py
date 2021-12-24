a42bcd06ba1c48791d915ec6df73143b1e55b1a0
Zhang_abab <2411358676@qq.com>
2021-12-24 13:52:27 +0800

    新增标题列表
    使用for循环列举出列表数据

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic
+
 def index(request):
 
     return render(request,'index.html')
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'topics.html', context)
 # Create your views here.
