9d5ceaad41b4102cd75a48e352628ea8a56c2d21
Zhang_abab <2411358676@qq.com>
2021-12-24 14:20:19 +0800

    添加文章详情页
    以文章id作为引索按照时间倒叙来显示文章

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -11,4 +11,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request, 'topics.html', context)
+
+def topic(request, topic_id):
+    """显示单个主题及其条目"""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries }
+    return render(request, 'topic.html', context)
 # Create your views here.
