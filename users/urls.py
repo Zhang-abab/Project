881d9cfbce7057614679017cf6b11191afb0b13f
Zhang_abab <2411358676@qq.com>
2021-12-24 19:49:07 +0800

    用户
    用户注册

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 """为users定义rul模式"""
 from typing import ParamSpec
 from django.urls import path, include
-
+from . import views
 app_name = 'users'
 
 urlpatterns = [
     #包含默认身份认证的URL，
     path('',include('django.contrib.auth.urls')),
+    #注册页面
+    path('register/', views.register,name='register')
 ]
